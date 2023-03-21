9fa2bbbf9503a482eea33ec41070a1d4c535eba2
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-03-21 17:38:30 -0600

    🆕 feat: Loading.

--- app/posts/[id]/comments/page.jsx
+++ app/posts/[id]/comments/page.jsx
@@ -1,5 +1,6 @@
 
-const fetchComments = (id) => {
+const fetchComments = async (id) => {
+  await new Promise(resolve => setTimeout(resolve, 3000))
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
     next: { revalidate: 60 }
   }).then(res => res.json())
@@ -9,12 +10,12 @@ export default async function Post ({ params }) {
   const { id } = params
   const comments = await fetchComments(id)
   return (
-    <ul>
+    <ul style={{ background: '#444', fontSize: '12px' }}>
       {
             comments.map(comment => (
               <li key={comment.id}>
-                <h2>{comment.name}</h2>
-                <p>{comment.body}</p>
+                <h4>{comment.name}</h4>
+                <small>{comment.body}</small>
               </li>
             ))
         }
